e555e00ce3ffa2da076471fb836b3875ca138745
Nicolás López <nicolaslopezj@me.com>
2014-12-03 02:49:36 -0300

    seo fixed

--- routes.js
+++ routes.js
@@ -7,23 +7,21 @@ Router.map(function() {
 			return [orion.subs.subscribe('dictionary')] 
 		},
 		onAfterAction: function() {
-			var dict;
 			if (!Meteor.isClient) {
 				return;
 			}
-			dict = orion.dictionary.collection.findOne();
 			SEO.set({
-				title: dict.seoTitle,
+				title: orion.dictionary.get('seoTitle'),
 				link: {
-					icon: dict.seoFavIcon.link,
+					icon: orion.dictionary.get('seoFavIcon.link'),
 				},
 				meta: {
-					'description': dict.seoDescription
+					'description': orion.dictionary.get('seoDescription')
 				},
 				og: {
-					'title': dict.seoTitle,
-					'description': dict.seoDescription,
-					'image': dict.seoImage.link
+					'title': orion.dictionary.get('seoTitle'),
+					'description': orion.dictionary.get('seoDescription'),
+					'image': orion.dictionary.get('seoImage.link')
 				}
 			});
 		}
